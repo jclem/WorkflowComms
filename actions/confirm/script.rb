1b1b9d0b32e0f35f486edb0be08104cf4c7d1ffa
Jonathan Clem <jclem@github.com>
2019-02-12 14:08:20 -0500

    Add more robust confirm interface

--- actions/confirm/script.rb
+++ actions/confirm/script.rb
@@ -54,24 +54,54 @@ class ActionConfirmation
 
     resp = Net::HTTP.post_form(uri, {
       channel: ENV["SLACK_BOT_CHANNEL"],
-      text: @msg_text,
       token: ENV["SLACK_BOT_TOKEN"],
       attachments: [
         {
-          text: "Choose 'Yes' to continue or 'Cancel' to cancel.",
+          title: @msg_text,
+          color: "good",
+          fields: [
+            {
+              "title": "Name",
+              "value": ENV["GITHUB_WORKFLOW"],
+              "short": true,
+            },
+            {
+              "title": "Repository",
+              "value": ENV["GITHUB_REPOSITORY"],
+              "short": true,
+            },
+            {
+              "title": "Event",
+              "value": ENV["GITHUB_EVENT_NAME"],
+              "short": true,
+            },
+            {
+              "title": "SHA",
+              "value": "https://github.com/#{ENV["GITHUB_REPOSITORY"]}/commit/#{ENV["GITHUB_SHA"]}",
+            },
+          ],
+        },
+        {
+          color: "warning",
           fallback: "Confirmation request failed.",
           callback_id: @callback_id,
           actions: [
             {
               name: "cancel",
-              text: "Cancel",
+              text: "Cancel Workflow",
               type: "button",
             },
             {
               name: "confirm",
-              text: "Yes",
+              text: "Continue Workflow",
               style: "danger",
               type: "button",
+              confirm: {
+                title: "Continue Workflow",
+                text: "Are you sure you want to continue this workflow?",
+                ok_text: "Continue Workflow",
+                dismiss_text: "Do Not Continue Workflow",
+              },
             },
           ],
         },
