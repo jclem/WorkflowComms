21900045bf8878eab4acba2017363dd785d8ea11
Jonathan Clem <jclem@github.com>
2019-02-13 19:28:54 -0500

    Simplify WorkflowComms dispatching

--- apps/workflow_comms/lib/workflow_comms.ex
+++ apps/workflow_comms/lib/workflow_comms.ex
@@ -4,38 +4,32 @@ defmodule WorkflowComms do
   action messages
   """
 
-  @message_providers %{
-    "slack" => WorkflowComms.MessageProvider.Slack,
-    "twilio" => WorkflowComms.MessageProvider.Twilio,
-    "test" => WorkflowComms.MessageProvider.Test
-  }
-
   alias WorkflowComms.Action
+  alias WorkflowComms.MessageProvider.{Slack, Twilio, Test}
 
   @doc """
   Handle an action.
   """
   @spec handle_action(Action.t()) :: :ok | {:error, term}
-  def handle_action(action) do
-    case @message_providers[action.provider] do
-      mod when is_atom(mod) ->
-        mod.handle_action(action)
-
-      _ ->
-        {:error, :no_such_provider}
-    end
-  end
+  def handle_action(%{provider: "slack"} = action), do: Slack.handle_action(action)
+  def handle_action(%{provider: "twilio"} = action), do: Twilio.handle_action(action)
+  def handle_action(%{provider: "test"} = action), do: Test.handle_action(action)
+  def handle_action(_), do: {:error, :no_such_provider}
 
   @doc """
   Store a callback and update its Slack message.
   """
   @spec handle_callback(String.t(), WorkflowComms.Storage.callback()) ::
           {:ok, Action.t()} | {:error, any}
-  def handle_callback(provider, %{"callback_id" => cb_id} = callback) do
-    with mod when is_atom(mod) <- @message_providers[provider],
-         {:ok, action} <- WorkflowComms.Storage.get_action(cb_id),
+  def handle_callback("slack", callback), do: do_handle_callback(Slack, callback)
+  def handle_callback("twilio", callback), do: do_handle_callback(Twilio, callback)
+  def handle_callback("test", callback), do: do_handle_callback(Test, callback)
+  def handle_callback(_, _), do: {:error, :no_such_provider}
+
+  defp do_handle_callback(provider, %{"callback_id" => cb_id} = callback) do
+    with {:ok, action} <- WorkflowComms.Storage.get_action(cb_id),
          action = Map.put(action, :callback, callback),
-         {:ok, action} <- mod.handle_callback(action, callback),
+         {:ok, action} <- provider.handle_callback(action, callback),
          {:ok, action} <- WorkflowComms.Storage.put_action(action) do
       {:ok, action}
     end
